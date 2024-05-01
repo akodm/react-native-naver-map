fada7213fe3dd949dae7b8fae446e03465d41811
akodm <a8456452@naver.com>
2024-05-01 12:35:53 +0900

    RN 73 버전 관련 문제 처리

--- ios/reactNativeNMap/RNNaverMapPathOverlayManager.m
+++ ios/reactNativeNMap/RNNaverMapPathOverlayManager.m
@@ -39,6 +39,6 @@ RCT_EXPORT_VIEW_PROPERTY(outlineColor, UIColor)
 RCT_EXPORT_VIEW_PROPERTY(passedOutlineColor, UIColor)
 RCT_EXPORT_VIEW_PROPERTY(pattern, NSString)
 RCT_EXPORT_VIEW_PROPERTY(patternInterval, CGFloat)
-RCT_EXPORT_VIEW_PROPERTY(onClick, RCTDirectEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onClick, RCTDirectEventBlock)
 
 @end
